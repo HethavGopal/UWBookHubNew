a82fc05dd1101e169b89e7393ab6d9ee6d522929
Hethav Gopal <fsad07@gmail.com>
2025-05-17 20:35:56 -0400

    single book page implement

--- frontend/src/redux/features/cart/booksAPI.js
+++ frontend/src/redux/features/cart/booksAPI.js
@@ -26,7 +26,38 @@ export const booksApi = createApi({
                 return response.books || [];
             },
         }),
+        fetchBookById: builder.query({
+            query: (id) => `/${id}`,
+            providesTags: (result, error, id) => [{ type: 'Books', id }],
+        }),
+        addBook: builder.mutation({
+            query: (newBook) => ({
+                url: '/create-book ',
+                method: 'POST',
+                body: newBook,   
+            }),
+            invalidatesTags: ['Books'],
+        }),
+        updateBook: builder.mutation({
+            query: ({ id, ...rest }) => ({
+                url: `/edit/${id}`,
+                method: 'PUT',
+                body: rest,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }),
+            invalidatesTags: ['Books'],
+        }),
+        deleteBook: builder.mutation({
+            query: (id) => ({
+                url: `${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Books'],
+        }),
     }),
 })
 
-export const { useFetchAllBooksQuery } = booksApi
+export const { useFetchAllBooksQuery, useFetchBookByIdQuery, useAddBookMutation, useUpdateBookMutation, useDeleteBookMutation } = booksApi
+export default booksApi;
